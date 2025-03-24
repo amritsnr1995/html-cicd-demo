890b2697d911133a7a12eb18e2dfb67d748d1a92
amrit-dev <amrit.snr1995@gmail.com>
2025-03-24 14:34:43 +0900

    fix cypress test location configuration

---
+++ cypress.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  e2e: {
+    baseUrl: 'http://localhost:8080',
+    supportFile: false,
+    fixturesFolder: false,
+    video: false,
+    specPattern: 'cypress/integration/*.spec.js',
+  },
+};
